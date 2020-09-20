0694e9b82fb89290fecabfcc0686451569f63022
Andrew Malchuk <andrew.malchuk@yandex.ru>
2020-09-20 04:31:43 +0400

    Sun, 20 Sep 2020 00:31:42 +0000

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 blackbox
 ========
-[![pipeline status][pipeline]][homepage]
-
-**Tor** and **Privoxy** bundle.
+**Squid** &#8594; **Privoxy** &#8594; **Tor** bundle.
 
 Usage
 -----
@@ -18,14 +16,7 @@ docker-compose up \
     --remove-orphans
 ```
 
-Distribution
-------------
-This project is licensed under the terms of the [MIT License](LICENSE).
-
 Links
 -----
 - Code: <https://gitlab.com/amalchuk/blackbox>
 - GitHub mirror: <https://github.com/amalchuk/blackbox>
-
-[homepage]: <https://gitlab.com/amalchuk/blackbox>
-[pipeline]: <https://gitlab.com/amalchuk/blackbox/badges/master/pipeline.svg?style=flat-square>
---
+++ squid/tordomains.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from http.client import HTTPSConnection
+from json.decoder import JSONDecoder
+from pathlib import Path
+from typing import List
+
+DEFAULT_ENCODING = "utf-8"
+
+
+def blacklisted_domains() -> List[str]:
+    """
+    Download the list of domains currently forbidden in the Russian Federation.
+    """
+    connection = HTTPSConnection("reestr.rublacklist.net")
+    connection.request("GET", "/api/v2/domains/json")
+    response_bytes = connection.getresponse().read()
+
+    response = response_bytes.decode(DEFAULT_ENCODING)
+    connection.close()
+
+    decoder = JSONDecoder()
+    return decoder.decode(response)
+
+
+if __name__ == "__main__":
+    tordomains_file = Path(__file__).with_name("tordomains.txt")
+    domains_list = blacklisted_domains()
+    print(f"Downloaded {len(domains_list):d} domains")
+
+    with tordomains_file.open("wt", encoding=DEFAULT_ENCODING) as io:
+        for domain in domains_list:
+            io.write(f"{domain}\n")
